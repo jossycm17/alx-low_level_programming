8a247a6d66345e46c7e9b1baeba332cf52680b63
Your Name <you@example.com>
2023-02-16 07:44:53 -0800

    commit 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main-  print a line with a put function
+ * return : 0 (success)
+ */
+int main(void)
+{
+puts("\"programming is like building  a mutilingual puzzle");
+return (0);
+}

5841fca58bcc99fd46f380439880197d00310278
Your Name <you@example.com>
2023-02-16 11:11:41 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float f;
 
 printf("size of a char: %ld byte(s)\n", sizeof(a));
-printf("size of a int: %ld byte(s)\n", sizeof(b));
+printf("size of an int: %ld byte(s)\n", sizeof(b));
 printf("size of a long int: %ld byte(s)\n", sizeof(c));
 printf("size of a long long int: %ld byte(s)\n", sizeof(d));
 printf("size of a float: %ld byte(s)\n", sizeof(f));

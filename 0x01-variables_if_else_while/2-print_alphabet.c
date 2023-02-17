add37a1b9773cd500ad0281454c55ee746d9cf6c
Your Name <you@example.com>
2023-02-17 05:21:47 -0800

    Q2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program to prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);

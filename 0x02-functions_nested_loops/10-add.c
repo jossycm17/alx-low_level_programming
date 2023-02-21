d04bd976c9452966aef2275d6b310a800282188f
Your Name <you@example.com>
2023-02-21 12:26:59 -0800

    5-11 c function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Add two values
+ * @n1: the first number to be computed
+ * @n2: the sedcond number to be computed
+ * Return: Intiger
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

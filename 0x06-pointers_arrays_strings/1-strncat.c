1a95b370eaaef64518ee347f9d1f6d1871ae0261
Your Name <you@example.com>
2023-03-02 11:45:23 -0800

    Q2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _strcat - Concatinate two string using at most
+ * _strncat - Concatinate two string using at most
  * @dest: The string to be appended upon
  * @src: the string to be appended on dest
  * @n: the number of byte from src to be appended to dest
  * Return: A pointer to the resulting string dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies at most an inputted number
+ * @dest: storing the string copy
+ * @src: the source string
+ * @n:the maximum number of byte copied
+ * Retun: A pointer to the result string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}

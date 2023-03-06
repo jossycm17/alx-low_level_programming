00988a26c9b051f9a723bdedf06289c9ee0cc47e
Your Name <you@example.com>
2023-03-06 10:31:24 -0800

    Q3-6

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strchr - Entry point 
+ * _strchr - Entry point
  * @s: input
- * @c: input 
+ * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
